27c67ddb0da11d4649e97149042b10d576efb83c
BartKo <bartoszkocmierowski@gmial.com>
2024-11-19 21:37:52 +0100

    x

---
+++ src/background.ts
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.webNavigation.onCompleted.addListener(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        if (id) {
+          chrome.action.disable(id);
+        }
+      });
+    }, { url: [{ hostContains: 'google.com' }] });
+  });
+
+
+console.log("background works")
